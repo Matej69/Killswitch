5d5efbedeb350b209c6dad98240425fa0668fbde
Matej69 <mlelek94@gmail.com>
2018-06-27 01:03:18 +0200

    FIrs GUI working example keept in game loop

--- Killswitch/App.cpp
+++ Killswitch/App.cpp
@@ -8,6 +8,9 @@
 #include "core\Sprite\Sprite.h"
 #include "core\Texture\Texture.h"
 #include "core\GameClock\GameClock.h"
+#include "core\imGUI\imgui.h"
+#include "core\imGUI\imgui_impl_glfw_gl3.h"
+
 
 using namespace std;
 int main(void)
@@ -32,17 +35,28 @@ int main(void)
 	if (glewInit() != GLEW_OK)
 		std::cout << "GLEW INIT ERROR";
 
+	// Setup ImGui
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(window, true);
+	ImGui::StyleColorsDark();
+
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	Sprite sprite = Sprite(0.1f, 0.1f, 0.5f, 0.5f, "res/textures/larvitar.png");
 	Sprite sprite2 = Sprite(2.0f, 2.0f, 0.0f, 0.0f, "res/textures/larvitar.png");
 
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		GameClock::OnFrameStart();
+		// imGUI stuf called at atart of every frame
+		ImGui_ImplGlfwGL3_NewFrame();
 
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -50,6 +64,30 @@ int main(void)
 		sprite.SetPosition(sprite.x + 0.0001f, sprite.y);
 		sprite.Render();
 		sprite2.Render();
+
+
+
+
+
+		static float f = 0.0f;
+		static int counter = 0;
+		ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+
+
+
+		// imGUI rendering
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -62,6 +100,8 @@ int main(void)
 		cout << GameClock::deltaTime << endl;
 	}
 
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 	glfwTerminate();
 	return 0;
 }
